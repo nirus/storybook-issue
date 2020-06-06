597a4df091362dbd61422d7033ef210bdd146f20
Nirus <nirus@live.in>
2020-06-06 21:34:59 +0530

    working configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,43 +4,36 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   stories: ['../src/stories/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
-  /*
-  // Not Working Config
+
   webpackFinal: async (config, { configType }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
     // 'PRODUCTION' is used when building the static version of storybook.
 
-    // Make whatever fine-grained changes you need
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../src'),
+    config.module.rules.map((rule) => {
+      if (rule.oneOf) {
+        rule.oneOf = rule.oneOf.slice().map((subRule) => {
+          if (subRule.test instanceof RegExp && subRule.test.test('.scss')) {
+            return {
+              ...subRule,
+              use: [
+                ...subRule.use,
+                {
+                  loader: require.resolve('sass-resources-loader'),
+                  options: {
+                    resources: [
+                      path.resolve(__dirname, '../src/styles/_common.scss')
+                    ]
+                  }
+                }
+              ],
+            }
+          }
+          return subRule;
+        });
+      }
+      return rule;
     });
-
-    // Return the altered config
     return config;
   },
-  */
-
-  
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    // Make whatever fine-grained changes you need
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../src'),
-    });
-
-    config.plugins.push(new MiniCssExtractPlugin({ filename: '[name].css' }));
-
-    // Return the altered config
-    return config;
-  },
-  
-  
 };
